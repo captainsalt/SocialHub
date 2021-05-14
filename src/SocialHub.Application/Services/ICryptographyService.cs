f55df7edeb9fe37acc6f80f3fd54e77422a2beff
David Wright <dontbothertheking@gmail.com>
2021-05-14 16:31:25 -0500

    Implement CryptographyService

---
+++ src/SocialHub.Application/Services/ICryptographyService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialHub.Application.Services
+{
+    public interface ICryptographyService
+    {
+        string Hash(string input);
+
+        bool IsMatch(string input, string hashedString);
+    }
+}
---
+++ src/SocialHub.Infrastructure/Services/CryptographyService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using SocialHub.Application.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialHub.Infrastructure.Services
+{
+    public class CryptographyService : ICryptographyService
+    {
+        private readonly int _iterations = 10_000;
+
+        public string Hash(string input)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(input, salt, _iterations);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            var hashed = Convert.ToBase64String(hashBytes);
+            return hashed;
+        }
+
+        public bool IsMatch(string input, string hashedString)
+        {
+            byte[] hashBytes = Convert.FromBase64String(hashedString);
+
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(input, salt, _iterations);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            for (int i = 0; i < 20; i++)
+                if (hashBytes[i + 16] != hash[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
