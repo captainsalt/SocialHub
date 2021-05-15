d058c84b35932a0ca7cfd42b696f09628c6a0550
David Wright <dontbothertheking@gmail.com>
2021-05-15 03:18:55 -0500

    Add AccountController

---
+++ src/SocialHub.API/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocialHub.API.Attributes;
+using SocialHub.Application;
+using SocialHub.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SocialHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var result = await _accountService.RegisterAsync(request);
+
+            if (result.IsRight)
+                return Ok();
+            else
+                return BadRequest();
+        }
+    }
+}
