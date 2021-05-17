661608e2424cda38b4829427ee9619db86f003cb
David Wright <dontbothertheking@gmail.com>
2021-05-16 22:42:51 -0500

    Add account to state

--- src/SocialHub.Web/src/views/Dashboard.vue
+++ src/SocialHub.Web/src/views/Dashboard.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
+    <div>Welcome {{ account.username }}</div>
     <div>This is the dashboard</div>
   </div>
 </template>
 
 <script lang="ts" setup>
+/* eslint-disable no-unused-vars, @typescript-eslint/no-unused-vars */
+import { account } from "@/store";
 </script>
