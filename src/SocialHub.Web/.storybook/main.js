9ff0f4d609d230807876763ffb0d1328ba745d6a
David Wright <dontbothertheking@gmail.com>
2021-05-19 11:43:49 -0500

    Add @ alias to storybook

--- src/SocialHub.Web/.storybook/main.js
+++ src/SocialHub.Web/.storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path")
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +8,13 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "../src/"),
+    };
+    config.resolve.extensions.push(".ts", ".js");
+    return config;
+  }
 }
