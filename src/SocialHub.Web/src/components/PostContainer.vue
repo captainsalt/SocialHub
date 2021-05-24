8364b0afbcd4654e6e855d80b3c32ded0bd91c57
David Wright <dontbothertheking@gmail.com>
2021-05-23 19:27:11 -0500

    PostContainer update

--- src/SocialHub.Web/src/components/PostContainer.vue
+++ src/SocialHub.Web/src/components/PostContainer.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="w-full border-2 border-purple-700 divide-y rounded-md">
-    <Post v-for="p in posts" :key="p.id" :post="p"/>
+    <Post
+      v-for="p in posts"
+      :key="p.id"
+      :post="p"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Post from "@/components/Post.vue";
 import PostModel from "@/models/PostModel";
-import { PropType } from "vue"
 
 export default {
   components: {
@@ -15,9 +18,9 @@ export default {
   },
   props: {
     posts: {
-      type: Array as PropType<Array<PostModel>>,
+      type: [],
       required: true,
-      validator: (val: PostModel[]) =>
+      validator: (val: any[]) =>
         val.every(item => item instanceof PostModel)
     }
   }
