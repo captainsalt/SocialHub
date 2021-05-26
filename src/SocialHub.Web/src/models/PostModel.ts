22637c010811ad4c310371eb01ff8c344dc8c880
David Wright <dontbothertheking@gmail.com>
2021-05-26 14:36:06 -0500

    Update frontend model

--- src/SocialHub.Web/src/models/PostModel.ts
+++ src/SocialHub.Web/src/models/PostModel.ts
@@ -1,15 +1,19 @@
 import AccountModel from "./AccountModel";
 
 export default class PostModel {
-  constructor(id: string, author: AccountModel, content: string, createdAt: Date) {
+  constructor(id: string, author: AccountModel, content: string, createdAt: Date, isLiked: boolean, isShared: boolean) {
     this.id = id;
     this.account = author;
     this.content = content;
     this.createdAt = createdAt;
+    this.isLiked = isLiked;
+    this.isShared = isShared;
   }
 
   id
   account
   content
   createdAt
+  isLiked
+  isShared
 }
