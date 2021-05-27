71436130f972aa402de9452b21147b719b27448c
David Wright <dontbothertheking@gmail.com>
2021-05-27 12:35:55 -0500

    Use svg components in BottomNav

--- src/SocialHub.Web/src/components/BottomNav.vue
+++ src/SocialHub.Web/src/components/BottomNav.vue
@@ -6,20 +6,7 @@
         class="nav-button"
         @click="$emit('onNav', '/home')"
       >
-        <svg
-          class="group-hover:text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
+        <HomeIcon class="group-hover:text-white"/>
 
         <span class="group-hover:text-white">
           Home
@@ -31,20 +18,7 @@
         class="nav-button"
         @click="$emit('onNav', `/profile/${account.username}`)"
       >
-        <svg
-          class="group-hover:text-white"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zm-4 7a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
+        <UserIcon class="group-hover:text-white"/>
 
         <span class="group-hover:text-white">
           Profile
@@ -56,8 +30,13 @@
 
 <script lang="ts">
 import { account } from "@/store";
+import { HomeIcon, UserIcon } from "@heroicons/vue/outline";
 
 export default {
+  components: {
+    HomeIcon,
+    UserIcon
+  },
   emits: {
     onNav: null
   },
