97a6e5eea6ffde9a7551e2c857bd4ccfb939b4f8
David Wright <dontbothertheking@gmail.com>
2021-05-28 16:05:06 -0500

    Add styling

--- src/SocialHub.Web/src/components/SideNav.vue
+++ src/SocialHub.Web/src/components/SideNav.vue
@@ -1,8 +1,49 @@
 <template>
-  <div>SideNav</div>
+  <div class="flex flex-col">
+    <a href="" class="nav-item group">
+      <HomeIcon class="nav-image "/>
+      <span class="nav-label ">Home</span>
+    </a>
+
+    <a href="" class="nav-item group">
+      <UserIcon class="nav-image "/>
+      <span class="nav-label ">Profile</span>
+    </a>
+  </div>
 </template>
 
 <script lang="ts">
+import { HomeIcon, UserIcon } from "@heroicons/vue/outline";
 export default {
+  components: {
+    HomeIcon,
+    UserIcon
+  }
 };
 </script>
+
+<style scoped>
+.nav-item {
+  @apply  p-5 bg-purple-500 flex  text-center items-center justify-items-center;
+}
+
+.nav-item:hover {
+  @apply bg-purple-600;
+}
+
+.nav-item:active {
+  @apply bg-purple-700 outline-none;
+}
+
+.nav-item:focus {
+  @apply outline-none;
+}
+
+.nav-image {
+  @apply h-7 group-hover:text-white
+}
+
+.nav-label {
+  @apply ml-4 text-xl group-hover:text-white group-hover:text-white;
+}
+</style>
