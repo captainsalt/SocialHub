3b5e89ba3fa3b164bbef0514bb6581d6ad4ab76a
David Wright <dontbothertheking@gmail.com>
2021-05-29 15:38:30 -0500

    Lint

--- src/SocialHub.Web/src/views/NavLayout.vue
+++ src/SocialHub.Web/src/views/NavLayout.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
     <div class="sm:grid-flow-col sm:grid-cols-12 sm:grid">
-      <div class="hidden sm:flex sm:justify-around sm:items-center sm:h-screen sm:col-span-4 sm:sticky sm:top-0 ">
-        <SideNav class="w-full max-w-sm mx-1 overflow-hidden bg-purple-100 rounded-sm h-1/3 "/>
+      <div class="hidden sm:flex sm:justify-around sm:items-center sm:h-screen sm:col-span-4 sm:sticky sm:top-0">
+        <SideNav class="w-full max-w-sm mx-1 overflow-hidden bg-purple-100 rounded-sm h-1/3"/>
       </div>
       <div class="sm:justify-center sm:col-span-8 sm:flex">
-        <router-view class="sm:w-2/3 sm:max-w-lg "/>
+        <router-view class="sm:w-2/3 sm:max-w-lg"/>
       </div>
     </div>
 
     <div class="fixed bottom-0 w-screen">
       <PostInput
         v-show="isHome"
-        class="px-4 mb-2"
+        class="hidden px-4 mb-2"
       />
       <BottomNav class="sm:hidden" @on-nav="changeView"/>
     </div>
