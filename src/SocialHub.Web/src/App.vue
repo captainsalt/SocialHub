7f6dd95779dda930c11583530ec3aa2a7a138106
David Wright <dontbothertheking@gmail.com>
2021-05-29 15:27:11 -0500

    Style scrollbar

--- src/SocialHub.Web/src/App.vue
+++ src/SocialHub.Web/src/App.vue
@@ -3,4 +3,23 @@
 </template>
 
 <style>
+* {
+  scrollbar-width: thin;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
 </style>
