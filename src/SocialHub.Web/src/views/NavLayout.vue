abe28f755499bbea2ef1909daddc8096dae6879b
David Wright <dontbothertheking@gmail.com>
2021-05-29 01:21:31 -0500

    Responsive SideNav

--- src/SocialHub.Web/src/views/NavLayout.vue
+++ src/SocialHub.Web/src/views/NavLayout.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <router-view/>
+    <div class="p-1 sm:grid-flow-col sm:grid-cols-12 sm:gap-1 sm:grid">
+      <SideNav class="hidden sm:col-span-3 sm:inline-block"/>
+      <router-view class="sm:col-span-9 sm:inline-block"/>
+    </div>
 
     <div ref="bNav" class="fixed bottom-0 w-screen">
       <PostInput
@@ -8,7 +11,7 @@
         class="px-4 mb-2"
         @error="setOffsetHeight"
       />
-      <BottomNav @on-nav="changeView"/>
+      <BottomNav class="sm:hidden" @on-nav="changeView"/>
     </div>
     <div ref="offsetDiv"/>
   </div>
@@ -20,6 +23,7 @@ import { onMounted, ref, watch } from "vue";
 import router from "@/router";
 import BottomNav from "@/components/BottomNav.vue";
 import PostInput from "@/components/PostInput.vue";
+import SideNav from "@/components/SideNav.vue";
 
 const bNav: Ref<HTMLDivElement> = ref(); // eslint-disable-line no-undef
 const offsetDiv: Ref<HTMLDivElement> = ref(); // eslint-disable-line no-undef
